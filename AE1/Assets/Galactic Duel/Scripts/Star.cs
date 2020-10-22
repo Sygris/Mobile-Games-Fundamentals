26add469c4326241026d2535c72d3da8a973a0a4
TiagoABVista <tiago.a.vista@gmail.com>
2020-10-22 14:41:23 +0100

    First Commit

---
+++ AE1/Assets/Galactic Duel/Scripts/Star.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    public float Speed;
+    public Transform Red;
+    public Transform Blue;
+
+    private bool GoTORedShip = false;
+
+
+    void Update()
+    {
+
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 RedPos = new Vector3(Red.position.x, 0, 0);
+        
+        if (transform.parent == null)
+        {
+            Vector3 BluePos = new Vector3(Blue.position.x, 0, 0);
+
+            if (GoTORedShip)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, RedPos, Speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, BluePos, Speed * Time.deltaTime);
+            }
+
+            CheckDirection(RedPos, BluePos);
+        }
+        else 
+        {
+            transform.position = RedPos;
+        }
+    }
+
+    void CheckDirection(Vector3 RedPos, Vector3 BluePos)
+    {
+        if ((transform.position == RedPos) && (GoTORedShip))
+        {
+            GoTORedShip = false;
+        }
+        else if ((transform.position == BluePos) && (!GoTORedShip))
+        {
+            GoTORedShip = true;
+        }
+    }
+}
+
---
+++ AE1/Assets/Menu/Scripts/LoadSceneButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadSceneButton : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        string sceneName = gameObject.GetComponentInChildren<Text>().text;
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
+
